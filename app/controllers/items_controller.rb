259a8b6ae4c23939cbcc9f919464ee29d074f4af
Elliot Strong <strong.elliot91@gmail.com>
2018-06-23 20:30:38 -0700

    edit/destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,22 @@ def create
   end
 end
 
+def edit
+end
+
+def update
+  if @item.update(item_params)
+    redirect_to item_path(@item)
+  else
+    render 'edit'
+  end
+end
+
+
+def destroy
+  @item.destroy
+  redirect_to root_path
+end
 
 
 def item_params
